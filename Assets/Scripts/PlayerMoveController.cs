29666925a918bce9f31d5473c2bd0352232ecb43
uynet <highsate@gmail.com>
2021-01-31 14:51:15 +0900

    playerMoveControllerを分離

---
+++ Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour {
+
+	public GameObject player;
+    public static float moveSpeed = 0.1f;
+    Vector3 up = new Vector3(0f,0f,moveSpeed);
+	Vector3 down= new Vector3(0f,0f,-moveSpeed);
+    Vector3 right= new Vector3(moveSpeed,0f,0f);
+    Vector3 left= new Vector3(-moveSpeed,0f,0f);
+
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            player.GetComponent<Player>().Move(up);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) 
+        {
+            player.GetComponent<Player>().Move(down);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) 
+        {
+            player.GetComponent<Player>().Move(left);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) 
+        {
+            player.GetComponent<Player>().Move(right);
+        }
+	}
+}
